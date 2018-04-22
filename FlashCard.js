8a2b360df0fa1d6328386f092848d5bf00fbf06c
gazdagergo <info@gazdagergo.com>
2018-04-22 06:47:16 +0200

    add native elements

--- FlashCard.js
+++ FlashCard.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { AppRegistry, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
+import { Card } from 'react-native-elements';
 
-export default Card = ({ question, answer }) => (
-  <View>
+export default FlashCard
+ = ({ question, answer }) => (
+  <Card>
     <Text>{ question }</Text>
     <Text>{ answer }</Text>
-  </View>
+  </Card>
 );
 
-Card.propTypes = {
+FlashCard.propTypes = {
   question: PropTypes.string,
   answer: PropTypes.string,
 };
--- ListOfCards.js
+++ ListOfCards.js
@@ -5,7 +5,11 @@ import {
   View,
   ActivityIndicator,
 } from 'react-native';
-import Card from './Card';
+import {
+  Header,
+  Card
+} from 'react-native-elements';
+import FlashCard from './FlashCard';
 
 export default class ListOfCards extends React.Component {
   state = {
@@ -38,8 +42,9 @@ export default class ListOfCards extends React.Component {
 
     return (
       <View>
+        <Header />
         {this.state.cards.map(card => (
-          card && <Card key={ card.id } { ...card } />
+          card && <FlashCard key={ card.id } { ...card } />
         ))}
       </View>
     );
