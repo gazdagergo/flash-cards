92a9c3ef161808285f18b50436ac1735fd8eca5c
gazdagergo <info@gazdagergo.com>
2018-04-22 06:04:27 +0200

    show question - answer

--- Card.js
+++ Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { AppRegistry, View, Text } from 'react-native';
 
-export default class Card extends React.Component {
-  render() {
-    return <Text>Card</Text>
-  }
-}
+export default Card = ({ question, answer }) => (
+  <View>
+    <Text>{ question }</Text>
+    <Text>{ answer }</Text>
+  </View>
+)
