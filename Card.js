2fa3f0d381e6250db379a0a4fbab6840598432da
gazdagergo <info@gazdagergo.com>
2018-04-22 06:12:17 +0200

    prop type check

--- Card.js
+++ Card.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { AppRegistry, View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default Card = ({ question, answer }) => (
   <View>
     <Text>{ question }</Text>
     <Text>{ answer }</Text>
   </View>
-)
+);
+
+Card.propTypes = {
+  question: PropTypes.string,
+  answer: PropTypes.string,
+};
